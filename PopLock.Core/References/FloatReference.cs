555ce0d2a806576b6dffc88786be8d0dd0bd112e
Joseph Schick <jschick04@gmail.com>
2020-09-27 13:35:10 -0500

    Refactored folder structure and created seperate project for core library

---
+++ PopLock.Core/References/FloatReference.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PopLock.Core.References {
+
+    [Serializable]
+    public class FloatReference {
+
+        public float constantValue;
+        public bool useConstant = true;
+        //public FloatVariable variable;
+
+    }
+
+}
